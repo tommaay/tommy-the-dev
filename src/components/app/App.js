ca9714c25bdd034b216afa4dea0e5b8b0bbe94ea
ToMMaaY <thuynh629@gmail.com>
2019-06-24 15:01:55 -0700

    initial redux setup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Header</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container text-center">
+            <header className="App-header">
+                <h1>Header</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers';
 
 import App from './components/app/App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
